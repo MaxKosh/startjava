5c40fbc396a92db776672456368f7c43a49b1df4
Max Koshelev <maxdiorio@yandex.ru>
2019-10-20 23:53:25 +0300

    implement HW2

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,28 +1,58 @@
 public class Wolf {
-	String sex;
-	String name;
-	int weight;
-	float height;
-	String color;
+	private String sex;
+	private String name;
+	private int weight;
+	private int age;
+	private String color;
 
 	public void toWalk() {
 
 	}
-
 	public void toRun() {
 
 	}
-
 	public void toSit() {
 
 	}
-
 	public String toHowl() {
 		return "Whoooooo";
 	}
-
 	public void toHunt() {
 
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		if (age > 8) {
+			System.out.println("Некорректный возраст.");
+		} else {
+			this.age = age;
+		}
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
 	}
 
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,22 +2,22 @@ public class WolfTest {
 	public static void main(String[] args) {
 		Wolf wolf = new Wolf();
 
-		wolf.name = "Wolfy";
-		wolf.sex = "male";
-		wolf.weight = 40;
-		wolf.height = 0.9f;
-		wolf.color = "grey";
+		wolf.setName("Wolfy");
+		wolf.setSex("male");
+		wolf.setWeight(40);
+		wolf.setAge(10);
+		wolf.setColor("grey");
 
 		wolf.toWalk();
 		wolf.toSit();
 		wolf.toRun();
 		wolf.toHunt();
 
-		System.out.println("Wolf name: " + wolf.name + "\nWolf sex: " + wolf.sex + 
-			"\nWolf weight: " + wolf.weight + "\nWolf height: " + wolf.height +
-			"\nWolf color: " + wolf.color);
+		System.out.println("Wolf name: " + wolf.getName() + "\nWolf sex: " + wolf.getSex() + 
+			"\nWolf weight: " + wolf.getWeight() + "\nWolf age: " + wolf.getAge() +
+			"\nWolf color: " + wolf.getColor());
 
-		System.out.println("How does the " + wolf.name + " howl?\n" + wolf.name + ": " + wolf.toHowl());
+		System.out.println("How does " + wolf.getName() + " howl?\n" + wolf.getName() + ": " + wolf.toHowl());
 
 	}
 }
