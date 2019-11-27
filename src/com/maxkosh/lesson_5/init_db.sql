431e9ae332183bfb7ee9c46e79ccd46474c7adb5
Max Koshelev <maxdiorio@yandex.ru>
2019-11-28 00:36:13 +0300

    implement HW5

--- src/com/maxkosh/lesson_5/init_db.sql
+++ src/com/maxkosh/lesson_5/init_db.sql
@@ -5,7 +5,7 @@ INSERT INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kai
 	('Horizon Brave',	'Mark-1', '72.54', '7.890', 'destroyed',	'China',	'22-DEC-2015',	'2'),
 	('Cherno Alpha',	'Mark-1', '85.34', '2.412', 'destroyed',	'Russia',	'01-JAN-2015',	'4'),
 	('Coyote Tango',	'Mark-1', '85.34', '2.312', 'destroyed',	'Japan',	'30-DEC-2015',	'1'),
-	('Gipsy Danger',	'Mark-3', '79.25', '1.980', 'destroyed',	'USA',	'10-JUL-2017',	'9'),
+	('Gipsy Danger',	'Mark-3', '79.25', '1.980', 'destroyed',	'USA',		'10-JUL-2017',	'9'),
 	('Striker Eureka',	'Mark-5', '76.20', '1.850', 'not destroyed',	'Australia',	'02-NOV-2019',	'11'),
 	('Crimson Typhoon',	'Mark-4', '76.20', '1.722', 'destroyed',	'China',	'22-AUG-2018',	'7'),
 	('Gipsy Avanger',	'Mark-6', '81.77', '2.004', 'not destroyed',	'Pan Pacific',	'12-JAN-2034',	'1'),
