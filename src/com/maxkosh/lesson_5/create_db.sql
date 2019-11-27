221f43c0cc91b8c94105e26cd420d1b22ee6b14c
Max Koshelev <maxdiorio@yandex.ru>
2019-11-28 00:20:17 +0300

    implement HW5

--- src/com/maxkosh/lesson_5/create_db.sql
+++ src/com/maxkosh/lesson_5/create_db.sql
@@ -1,12 +1,12 @@
 -- creating table
 CREATE TABLE Jaegers (
-id				SERIAL PRIMARY KEY,
-modelName		TEXT,
-mark			CHAR(6),
-height			FLOAT(4),
-weight			FLOAT(4),
-status			TEXT,
-origin			TEXT,
-launch			DATE,
-kaijuKill		INTEGER
-);
+	id				SERIAL PRIMARY KEY,
+	modelName		TEXT,
+	mark			CHAR(6),
+	height			FLOAT(4),
+	weight			FLOAT(4),
+	status			TEXT,
+	origin			TEXT,
+	launch			DATE,
+	kaijuKill		INTEGER
+	);
