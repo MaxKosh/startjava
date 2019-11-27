b4676fbbf6b43903d4fc6b9723939feda9f9adc5
Max Koshelev <maxdiorio@yandex.ru>
2019-11-27 22:51:52 +0300

    implement HW4

---
+++ src/com/maxkosh/lesson_5/create_db.sql
@@ -0,0 +1,13 @@
+-- creating table
+
+CREATE TABLE Jaegers (
+id				SERIAL PRIMARY KEY,
+modelName		TEXT,
+mark			CHAR(6),
+height			FLOAT(4),
+weight			FLOAT(4),
+status			TEXT,
+origin			TEXT,
+launch			DATE,
+kaijuKill		INTEGER
+);
---
+++ src/com/maxkosh/lesson_5/querise_db.sql
@@ -0,0 +1,21 @@
+--querise database
+
+SELECT * FROM Jaegers;
+
+SELECT * FROM Jaegers WHERE status = 'not destroyed';
+
+SELECT * FROM Jaegers WHERE mark = 'Mark-1' OR mark = 'Mark-6';
+
+SELECT * FROM Jaegers ORDER BY mark DESC;
+
+SELECT * FROM Jaegers WHERE launch = (SELECT MIN(launch) FROM Jaegers);
+
+SELECT * FROM Jaegers WHERE kaijuKill = (SELECT MIN(kaijuKill) FROM Jaegers);
+
+SELECT * FROM Jaegers WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM Jaegers);
+
+SELECT AVG(weight) FROM Jaegers;
+
+UPDATE Jaegers SET kaijuKill = kaijuKill + 1 WHERE status = 'not destroyed';
+
+DELETE FROM Jaegers WHERE status = 'destroyed';
